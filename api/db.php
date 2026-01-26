d26984209f61e71a0f4f86563473d73f1fa4488c
Yu <hank76421@gmail.com>
2026-01-26 13:29:33 +0800

    切版 & db第一次跟打

--- api/db.php
+++ api/db.php
@@ -4,63 +4,138 @@ session_start();
 
 class DB{
     protected $dsn="mysql:host=localhost;dbname=db03;charset=utf8";
-    protected $pdo
-    protected $table
+    protected $pdo;
+    protected $table;
 
 
 function __construct($table){
     $this->table=$table;
-    $this>pdo=new PDO(dsn,'root','');
+    $this->pdo=new PDO($this->dsn,'root','');
+}
+
+function all(...$arg){
+    $sql="SELECT * FROM $this->table ";
+        if(isset($arg[0])){
+            if(is_array[$arg[0]]){
+                $where=$this->array2sql($arg[0]);
+                $sql .= " WHERE ".join(" AND ",$where);
+            }else{
+                $sql .=$arg[0];
+            }
+        }
+
+        if(isset($arg[1])){
+            $sql .=$arg[1];
+        }
+    return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 }
 
 function find($id){
     $sql="SELECT * FROM $this->table ";
 
-    return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    if(is_array($id)){
+        $where=$this->array2sql($id);
+        $sql .= " WHERE ".join(" AND ",$where);
+    }else{
+        $sql .= " WHERE `id`='{$id}'";
+    }
+    return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+
 }
 
 
 function save($array){
-    $sql="SELECT * FROM $this->table ";
+    if(isset($array['id'])){
+        $set=$this->array2sql($array);
+        $sql="UPDATE $this->table SET ".join(",",$set). " WHERE `id`='{$array['id']}'";
+    }else{
+        $cols=array_keys($array);
+        $sql="INSERT INTO $this->table (`".join("`,`",$cols)."`) VALUES('".join("','",$array)."')";
+    }
 
-    return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    return $this->pdo->exec($sql);
 }
 
 
 function del($id){
-    $sql="SELECT * FROM $this->table ";
+    $sql="DELETE FROM $this->table ";
 
-    return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    if(is_array($id)){
+        $where=$this->array2sql($id);
+        $sql .= " WHERE ".join(" AND ",$where);
+    }else{
+        $sql .= " WHERE `id`='{$id}'";
+    }
+
+    return $this->pdo->exec($sql);
 }
 
 
 function count(...$arg){
-    $sql="SELECT * FROM $this->table ";
+    $sql="SELECT count(*) FROM $this->table ";
+        if(isset($arg[0])){
+            if(is_array($arg[0])){
+                $where=$this->array2sql($arg[0]);
+                $sql .= " WHERE ".join(" AND ",$where);
+            }else{
+                $sql .=$arg[0];
+            }
+        }
 
-    return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        if(isset($arg[1])){
+            $sql .= $arg[1];
+        }
+    return $this->pdo->query($sql)->fetchColumn();
 }
 
 
 function sum($col,...$arg){
-    $sql="SELECT * FROM $this->table ";
+    $sql="SELECT sum(`$col`) FROM $this->table ";
+        if(isset($arg[0])){
+            if(is_array($arg[0])){
+                $where=$this->array2sql($arg[0]);
+                $sql .= " WHERE ".join(" AND ",$where);
+            }else{
+                $sql .=$arg[0];
+            }
+        }
 
-    return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+        if(isset($arg[1])){
+            $sql .= $arg[1];
+        }
+    return $this->pdo->query($sql)->fetchColumn();
 }
+
+
+private function max($col,...$arg){
+    $sql="SELECT max(`$col`) FROM $this->table ";
+        if(isset($arg[0])){
+            if(is_array($arg[0])){
+                $where=$this->array2sql($arg[0]);
+                $sql .= " WHERE ".join(" AND ",$where);
+            }else{
+                $sql .=$arg[0];
+            }
+        }
+
+        if(isset($arg[1])){
+            $sql .= $arg[1];
+        }
+
+    return $this->pdo->query($sql)->fetchColumn();
+} 
 
 
 private function array2sql($array){
-    $sql="SELECT * FROM $this->table ";
+    $tmp=[];
+    foreach($array as $key => $value){
+        $tmp[]="`$key`='$value'";
+    }
+    
 
-    return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    return $tmp;
 }
 
-}
-
-
-function q($array){
-    $dsn="mysql:host=localhost;dbname=db03;charset=utf8";
-    $pdo=new PDO($this->dsn,'root','');
-    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 }
 
 
@@ -68,12 +143,15 @@ function to($url){
     header("location:".$url);
 }
 
+function q($sql){
+    $dsn="mysql:host=localhost;dbname=db03;charset=utf8";
+    $pdo=new PDO($this->dsn,'root','');
+    return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+}
 
-$Title=new DB['title'];
-$Mvim=new DB['mvim'];
-$=new DB[''];
-$=new DB[''];
-$=new DB[''];
-$=new DB[''];
-$=new DB[''];
-$=new DB[''];
+
+
+
+
+$Poster=new DB('posters');
+$Movie=new DB('movies');
