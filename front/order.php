6c3498313c95626f8d6fa78671f3c3890d4642b8
Josh12903 <hank76421@gmail.com>
2026-02-05 01:15:50 +0800

    Block ai 再整理預覽

--- front/order.php
+++ front/order.php
@@ -1,3 +1,4 @@
+<div id="orderform">
 <h3 class="ct">線上訂票</h3>
 
 <style>
@@ -10,6 +11,7 @@
     }
 </style>
 
+<form>
 <table id="orderL">
     <tr>
         <td>電影：</td>
@@ -46,15 +48,47 @@
         </td>
     </tr>
 </table>
-
-
+                <div class="ct">
+                    <button class="send-order">確定</button>
+                    <button type="reset">重置</button>
+                </div>
+</div>
+</form>
+<div id="seat" style="display:none">
+    <button class="prev-step">上一步</button>
+    <button class="order-btn">訂購</button>
+</div>
+<div id="orderResult" style="display:none"></div>
 <script>
+
+
     $("#movie").on("change",function(){
         let movieId=$(this).val();
         selectDate(movieId);
     })
 
         selectDate(("#movie").val());
+
+// 邏輯 分類再整理
+$(".send-order").on("click",function(){
+    $("#seat").show();
+    $("#orderform").hide();
+    $("#orderResult").hide();
+})
+$(".prev-order").on("click",function(){
+    $("#seat").hide();
+    $("#orderform").show();
+    $("#orderResult").hide();
+})
+$(".order-btn").on("click",function(){
+    $("#seat").hide();
+    $("#orderform").show();
+    $("#orderResult").show();
+})
+
+// $(".reset").on("click",function(){
+//     $("#movie").val($("#movie option").eq(0).val());
+// })
 
     function selectDate(movieId){
         $.get("api/get_date.php",{movieId},function(dates){
